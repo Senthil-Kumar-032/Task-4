343d0edd3a04fb4d30b94ce8c14afab0e0806483
Senthil Kumar S <senthilskumar032@gmail.com>
2026-02-05 01:54:02 +0530

    Task 1 - Question 1

---
+++ main.py
@@ -0,0 +1,21 @@
+# Guess the Number game
+
+# number to be guessed
+number = 3
+
+print("Guess a number between 1 and 10")
+
+# Program runs until correct guess
+while True:
+    guess = int(input("Enter your guess: "))
+
+    # checks the number is too high or too low or correct
+    if guess < number:
+        print("Too low")
+    elif guess > number:
+        print("Too high")
+    # If the number is correct it will break the loop
+    else:
+        print("Correct Number Found")
+        break
+
